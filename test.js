f5c3473f7e6a8aa3b75ff5aeaaa35151c19b86ca
github-cloudlabsuser-1962 <github_cloudlabsuser_1962@cloudlabsaiuser.com>
2024-05-15 15:59:32 +0000

    Copilot commit

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ function calculator(operation, num1, num2) {
     let result;
 
     switch (operation) {
-       case 'add':
+        case 'add':
             result = num1 + num2;
             break; 
         case 'subtract':
